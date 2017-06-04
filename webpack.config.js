ea342488aec6eac64e9e81bd4a5bcb4dd37b91fd
INOUE Takuya <inouetakuya5@gmail.com>
2017-06-04 18:30:40 +0900

    Rename src/index.js -> src/main.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'vue-nl2br.js',
