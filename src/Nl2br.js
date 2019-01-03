e38ca5eabe0965cef74751dd6fc345833cd7a05f
INOUE Takuya <inouetakuya5@gmail.com>
2019-01-03 16:07:16 +0900

    Add className to props

--- src/Nl2br.js
+++ src/Nl2br.js
@@ -9,14 +9,19 @@ export default {
       type: String,
       required: true,
     },
+    className: {
+      type: String,
+      required: false,
+    },
   },
-  render (createElement, context) {
-    return createElement(context.props.tag,
-      context.props.text.split('\n').reduce((accumulator, string) => {
-        if (!Array.isArray(accumulator)) {
-          return [accumulator, createElement('br'), string]
-        }
-        return accumulator.concat([createElement('br'), string])
-      }))
+  render(createElement, context) {
+    return createElement(context.props.tag, {
+      'class': context.props.className
+    }, context.props.text.split('\n').reduce((accumulator, string) => {
+      if (!Array.isArray(accumulator)) {
+        return [accumulator, createElement('br'), string]
+      }
+      return accumulator.concat([createElement('br'), string])
+    }))
   },
 }
