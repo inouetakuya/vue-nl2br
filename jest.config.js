a9b8eff0aec3daaf282965fce547699912b37592
INOUE Takuya <inouetakuya5@gmail.com>
2022-06-11 01:55:16 +0900

    test: testMatch: ['**/__tests__/**/*.(spec|test).[jt]s']

--- jest.config.js
+++ jest.config.js
@@ -158,6 +158,7 @@ module.exports = {
   //   "**/__tests__/**/*.[jt]s?(x)",
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
+  testMatch: ['**/__tests__/**/*.(spec|test).[jt]s'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
