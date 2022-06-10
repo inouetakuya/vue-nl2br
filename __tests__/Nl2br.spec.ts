b5fc1048d43ef8485540b45f124533ee0a413c0d
INOUE Takuya <inouetakuya5@gmail.com>
2022-06-11 00:55:54 +0900

    test: mount Nl2br and test it

--- __tests__/Nl2br.spec.ts
+++ __tests__/Nl2br.spec.ts
@@ -1,5 +1,18 @@
+import { mount } from '@vue/test-utils'
+import Nl2br from '../src/Nl2br'
+
 describe('Nl2br', () => {
-  test('true', () => {
-    expect(true).toBe(true)
+  const wrapper = mount(Nl2br, {
+    context: {
+      props: {
+        tag: 'p',
+        text: 'myLine\nmyLine2',
+        className: 'foo bar',
+      },
+    },
+  })
+
+  test('create br elements', () => {
+    expect(wrapper.html()).toBe('<p class="foo bar">myLine<br>myLine2</p>')
   })
 })
