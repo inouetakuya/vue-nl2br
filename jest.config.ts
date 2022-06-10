03f98fd840f95c19a7ab084e15c41e359f3e68e5
INOUE Takuya <inouetakuya5@gmail.com>
2022-06-11 04:11:22 +0900

    chore: use TypeScript for Jest configuration file

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,9 @@
 /*
- * For a detailed explanation regarding each configuration property, visit:
+ * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
 
-// eslint-disable-next-line no-undef
-module.exports = {
+export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
